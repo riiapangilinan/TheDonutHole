3d3a83d9d7efce179a371b3440c83219315c98f4
riiapangilinan <riiapangilinan@gmail.com>
2025-07-11 21:14:27 +0800

    added about and contact pages

--- main.py
+++ main.py
@@ -24,6 +24,14 @@ def login_required(f):
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
